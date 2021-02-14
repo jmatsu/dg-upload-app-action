03a634aa3fc668c8fabce97de6d6da25233cede4
Jumpei Matsuda <jmatsu.drm@gmail.com>
2021-02-14 22:48:26 +0900

    fix(*): Set maximum limit when creating a client

--- src/deploygate/client.ts
+++ src/deploygate/client.ts
@@ -44,6 +44,8 @@ export const createAxiosInstance: AxiosFactory = (baseURL, token) => {
     headers: {
       Authorization: `token ${token}`,
     },
+    maxBodyLength: 5 * 1024 * 1024 * 1024,
+    maxContentLength: 5 * 1024 * 1024 * 1024,
   });
 };
 
@@ -109,8 +111,6 @@ export const createClient: (options: ClientOptions) => Client = options => {
 
       const request = instance.post(`/users/${appOwnerName}/apps`, form, {
         headers: form.getHeaders(),
-        maxBodyLength: 5 * 1024 * 1024 * 1024,
-        maxContentLength: 5 * 1024 * 1024 * 1024,
       });
       return normalizeResponse(request);
     },
